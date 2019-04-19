a00291fcd0c8a080d0d4e5c579237411554bfc10
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-19 16:31:50 +0000

    rounding the users input to a whole number to work with, dividing the change by 25 and adding to a counter for how many times divisible to find quarters. Will do the same logic with other coin values.

--- cash.c
+++ cash.c
@@ -1,11 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
+//     intialize variables so they are availabe in the global scope
+    int counter;
     float change;
+    int cents;
+    int remanider; 
+    
     do {
+//       get input from customer - amount of change due
         change = get_float("Change Due: ");
     }
+//         if user enters a negative number re ask for the amount of change due
     while (change <= 0);
-        printf("%f\n", change);
-}
+//      printf("%f\n", change);
+//         round the change off into a whole number
+        cents = round(change * 100);
+        printf("%i\n", cents);
+    
+        counter = 0;
+        while ((cents / 25) > 0) {
+            
+            cents -= 25;
+            counter++;
+        }
+        printf("%i\n", counter);
+}    
