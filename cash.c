472e39fc7174aeeee64e0e64b7d8d642c91adc0d
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-22 03:19:12 +0000

    now checking dimes against the remaining change, and adding to counter accordingly

--- cash.c
+++ cash.c
@@ -7,7 +7,7 @@ int main(void) {
     int counter;
     float change;
     int cents;
-    int remanider; 
+    int remainder; 
     
     do {
 //       get input from customer - amount of change due
@@ -18,13 +18,20 @@ int main(void) {
 //      printf("%f\n", change);
 //         round the change off into a whole number
         cents = round(change * 100);
-        printf("%i\n", cents);
+//         printf("%i\n", cents);
     
         counter = 0;
         while ((cents / 25) > 0) {
-            
             cents -= 25;
             counter++;
         }
+        remainder = cents % 25;
+        printf("%i\n", remainder);
+//         printf("%i\n", counter);
+        while ((remainder / 10) > 0) {
+            cents -= 10;
+            counter++;
+        }
         printf("%i\n", counter);
-}    
+        printf("%i\n", cents);
+}        
